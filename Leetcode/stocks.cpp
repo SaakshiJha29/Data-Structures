f6317fcee22534a377d1538ed15ba703f72c4bdf
Saakshi <saakshi5454@gmail.com>
2026-02-16 23:59:29 +0530

    updated sqrt 69

---
+++ Leetcode/stocks.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = min(minPrice, price);
+            maxProfit = max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+};
+
+int main() {
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    Solution obj;
+    int result = obj.maxProfit(prices);
+
+    cout << "Max Profit: " << result << endl;
+    return 0;
+}
