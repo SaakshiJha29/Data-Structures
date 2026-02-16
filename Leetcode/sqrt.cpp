7e0412ff7a4e347442bd9e89ccf82a5fc90e471b
Saakshi <saakshi5454@gmail.com>
2026-02-16 13:12:03 +0530

    updated sqrt 69

--- Leetcode/sqrt.cpp
+++ Leetcode/sqrt.cpp
@@ -6,20 +6,20 @@ public:
     int mySqrt(int x) {
         if (x == 0 || x == 1) return x;
 
-        long left = 1, right = x;
+        long l = 1, r = x;
         int ans = 0;
 
-        while (left <= right) {
-            long mid = left + (right - left) / 2;
+        while (l <= r) {
+            long mid = l + (r - l) / 2;
 
             if (mid * mid == x)
                 return mid;
             else if (mid * mid < x) {
                 ans = mid;
-                left = mid + 1;
+                l = mid + 1;
             } 
             else {
-                right = mid - 1;
+                r = mid - 1;
             }
         }
         return ans;
