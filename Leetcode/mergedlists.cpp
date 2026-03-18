cb70fd2c7c4ee86e8a5d61f6cef77a3a2f512b40
Saakshi <saakshi5454@gmail.com>
2026-03-18 19:42:53 +0530

    merged two sorted lists

---
+++ Leetcode/mergedlists.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy(-1);
+        ListNode* temp = &dummy;
+
+        while (list1 && list2) {
+            if (list1->val <= list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+
+        // Attach remaining nodes
+        if (list1) temp->next = list1;
+        else temp->next = list2;
+
+        return dummy.next;
+    }
+};
